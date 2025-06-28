fc9389c58a6b3788c92c8c111afcca5d530c5aaf
Carlos Alejandro Subauste Marca <52093179+charlie2087390148@users.noreply.github.com>
2025-06-27 21:06:55 -0500

    Adicionando controles
    Se agregaron mas controles al formulario como radio butons hiddens y campos checkbox

--- Jakartawebappform/src/main/java/org/csubuaste/java/apiservlet/webapp/FormServlet.java
+++ Jakartawebappform/src/main/java/org/csubuaste/java/apiservlet/webapp/FormServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -18,20 +19,46 @@ public class FormServlet extends HttpServlet {
         String usename = req.getParameter("username1");
         String pass  = req.getParameter("password1");
         String email = req.getParameter("email");
+        String pais = req.getParameter("pais1");
+        String[] lenguajes = req.getParameterValues("lenguajes1");
+        String[] roles = req.getParameterValues("roles");
+
+        String idioma = req.getParameter("idioma");
+        String Habilitar = req.getParameter("habilitar");
+        String secreto = req.getParameter("secreto");
         try(PrintWriter out = resp.getWriter()){
-        out.print("<!DOCTYPE html>");
-        out.print("<head>");
-        out.print("    <meta charset=\"UTF-8\">");
-        out.print("    <title></title>");
-        out.print("</head>");
-        out.print("<body>");
-        out.print("    <ul>");
-        out.print("     <li>Usename : "+usename+"</li>");
-        out.print("     <li>password : "+pass+"</li>");
-        out.print("     <li>email : "+email+"</li>");
-        out.print("    </ul>");
-        out.print("</body>");
-        out.print("</html>");
+            out.print("<!DOCTYPE html>");
+            out.print("<head>");
+            out.print("    <meta charset=\"UTF-8\">");
+            out.print("    <title></title>");
+            out.print("</head>");
+            out.print("<body>");
+            out.print("    <ul>");
+            out.print("     <li>Usename : "+usename+"</li>");
+            out.print("     <li>password : "+pass+"</li>");
+            out.print("     <li>email : "+email+"</li>");
+            out.print("     <li>pais : "+pais+"</li>");
+            out.println("   <li><ul>Lenguajes: ");
+
+            Arrays.asList(lenguajes).forEach(lenguaje->{
+                out.println("   <li>"+lenguaje+"</li>");
+            });
+
+            out.println("   </li></ul>");
+
+            out.println("   <li><ul>Roles:");
+
+            Arrays.asList(roles).forEach(role->{
+                out.println("   <li>"+role+"</li>");
+            });
+
+            out.println("   </li></ul>");
+            out.print("     <li>idioma : "+idioma+"</li>");
+            out.print("     <li>habilitar : "+Habilitar+"</li>");
+            out.print("     <li>secreto : "+secreto+"</li>");
+            out.print("    </ul>");
+            out.print("</body>");
+            out.print("</html>");
 
         }catch(Exception ex){
 
